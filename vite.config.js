27b17949b8b92309c736560538636b458311df70
MisterFoxyDev <misterfoxydev@gmail.com>
2024-09-30 13:49:42 +0200

    modif config Vite pour régler erreur MIME

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
 });
